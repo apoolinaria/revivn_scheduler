2b00abb2b6155102fd9ccc92c79fa2c0c90f9a41
Polina <chebanenkopolina@gmail.com>
2021-10-21 23:27:10 -0400

    schema created

---
+++ schema.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS schedules;
+CREATE DATABASE schedules;
+
+USE schedules
+
+CREATE TABLE pickups (
+  id INT NOT NULL AUTO_INCREMENT,
+  location_name varchar (100) NOT NULL,
+  address varchar(150) NOT NULL,
+  date_scheduled DATETIME,
+  notes varchar(600),
+  status INT NOT NULL
+)
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  email varchar (100) NOT NULL,
+  first_name varchar(50) NOT NULL,
+  last_name varchar(50) NOT NULL,
+  company varchar(150) NOT NULL,
+)
